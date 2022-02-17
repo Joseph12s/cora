a095a47224490119ef80c2f54d19fc32013cd8ba
Joseph12s <81655028+Joseph12s@users.noreply.github.com>
2022-02-16 22:28:12 -0600

    Add files via upload

---
+++ Corazon.py
@@ -0,0 +1,17 @@
+from turtle import*
+
+def curvemove()
+  for i in range(200)
+  righ(1)
+  forward(1)
+
+color('red','pink')
+begin_fill()
+left(140)
+forward(111.65)
+curvemove()
+left(140)
+curvemove()
+forward(111.65)
+end_fill()
+done()
